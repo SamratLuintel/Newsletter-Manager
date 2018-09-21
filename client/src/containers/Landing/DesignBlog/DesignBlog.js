65f6e17f957f2218addbae03c94856b4aff13636
samratluitel <ngc2058@gmail.com>
2018-09-21 18:09:13 +0545

    Design Blog section finished on landing page

---
+++ client/src/containers/Landing/DesignBlog/DesignBlog.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DesignBlog = () => {
+  return (
+    <div className="DesignBlog">
+      <div className="DesignBlog__designer-background" />
+      <div className="DesignBlog__content">
+        <h2 className="DesignBlog__title">
+          Become an even better email designer
+        </h2>
+        <p className="DesignBlog__description">
+          Join 20,000 email designers and marketers that regularly visit our
+          email design blog to get inspired. Learn new EMM tricks and keep you
+          up to date on cool email design trends.
+        </p>
+        <a className="DesignBlog__button">CHECK OUT THE BLOG</a>
+      </div>
+    </div>
+  );
+};
+
+export default DesignBlog;
