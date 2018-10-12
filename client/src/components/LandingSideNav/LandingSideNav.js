3455d20ce3bf239adbfecf7409fde7c859d9561c
samratluitel <ngc2058@gmail.com>
2018-10-12 18:01:59 +0545

    Get In Touch Page made responsive

--- client/src/components/LandingSideNav/LandingSideNav.js
+++ client/src/components/LandingSideNav/LandingSideNav.js
@@ -2,13 +2,28 @@ import { slide as Menu } from "react-burger-menu";
 
 import React, { Component } from "react";
 
+// Other styles are in LandingSideNav
+const darkStyles = {
+  bmBurgerBars: {
+    background: "#179dc7"
+  }
+};
+
+const lightStyles = {
+  bmBurgerBars: {
+    background: "#fff"
+  }
+};
 class LandingSideNav extends Component {
   showSettings(event) {
     event.preventDefault();
   }
   render() {
     return (
-      <Menu className="LandingSideNav">
+      <Menu
+        styles={this.props.dark ? darkStyles : lightStyles}
+        className="LandingSideNav"
+      >
         <a id="home" className="bm-item-list" href="/">
           Home
         </a>
--- client/src/containers/GetInTouch/GetInTouch.js
+++ client/src/containers/GetInTouch/GetInTouch.js
@@ -3,12 +3,15 @@ import PageTitle from "./PageTitle/PageTitle";
 import DesktopNav from "../../components/DesktopNav/DesktopNav";
 import ContactForm from "./ContactForm/ContactForm";
 import Social from "./Social/Social";
+import LandingSideNav from "../../components/LandingSideNav/LandingSideNav";
 
 class GetInTouch extends Component {
   render() {
     return (
       <div className="GetInTouch">
+        <LandingSideNav dark />
         <DesktopNav dark />
+        <div className="GetInTouch__white-area" />
         <PageTitle />
         <ContactForm />
         <Social />
