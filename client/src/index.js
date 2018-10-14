1a49b4df79f217e0bd39780b26f61c6410f98dd4
samratluitel <ngc2058@gmail.com>
2018-10-14 17:18:15 +0545

    Redux changed for production build

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore,applyMiddleware } from 'redux';
-import reducers from './store/reducers/reducersIndex';
-import reduxThunk from 'redux-thunk';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./store/reducers/reducersIndex";
+import reduxThunk from "redux-thunk";
+import "./index.scss";
 
-const store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),applyMiddleware(reduxThunk))
+// const store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),applyMiddleware(reduxThunk))
 
+const store = createStore(reducers, applyMiddleware(reduxThunk));
 ReactDOM.render(
-    <div>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </div>
-    ,
-    document.getElementById('root'));
+  <div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </div>,
+  document.getElementById("root")
+);
 registerServiceWorker();
