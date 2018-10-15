5c6ec17b84b1c289905d80aaef4ae5a6881a6cc5
samratluitel <ngc2058@gmail.com>
2018-10-15 20:16:22 +0545

    o auth https forcefully changed to http

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.use(function(req, res, next) {
   if (process.env.NODE_ENV === "production") {
     const reqType = req.headers["x-forwarded-proto"];
     // if not https redirect to https unless logging in using OAuth
-    if (reqType !== "https") {
+    if (reqType !== "http") {
       req.url.indexOf("auth/google") !== -1
         ? next()
-        : res.redirect("https://" + req.headers.host + req.url);
+        : res.redirect("http://" + req.headers.host + req.url);
     }
   } else {
     next();
