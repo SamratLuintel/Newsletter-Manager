c2da08653ad851c936e51f8151dc0f676b159748
samratluitel <ngc2058@gmail.com>
2018-10-16 11:53:31 +0545

    register service worker deleted

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
@@ -22,4 +21,3 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
-registerServiceWorker();
