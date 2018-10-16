cc73397e07a3e6aed6431b05b4b263b41093078d
samratluitel <ngc2058@gmail.com>
2018-10-16 11:55:05 +0545

    Programmatically removing register service worker

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware } from "redux";
 import reducers from "./store/reducers/reducersIndex";
 import reduxThunk from "redux-thunk";
 import "./index.scss";
+import { unregister } from "./registerServiceWorker";
 
 // const store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),applyMiddleware(reduxThunk))
 
@@ -21,3 +22,4 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+unregister();
